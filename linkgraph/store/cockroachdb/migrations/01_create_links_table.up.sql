6495a3161c856d2cdaf4f607961140715e8e9cc1
KyteProject <headhertz88@gmail.com>
2021-05-22 14:06:21 +0100

    update formating

--- linkgraph/store/cockroachdb/migrations/01_create_links_table.up.sql
+++ linkgraph/store/cockroachdb/migrations/01_create_links_table.up.sql
@@ -1,6 +1,5 @@
-CREATE TABLE IF NOT EXISTS links
-(
-    id           UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    url          STRING UNIQUE,
+CREATE TABLE IF NOT EXISTS links (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    url STRING UNIQUE,
     retrieved_at TIMESTAMP
 );
